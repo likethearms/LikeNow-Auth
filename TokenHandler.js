f06c058e82b7558795784d37e052ad4a9f50e0ef
Henrik Salmela <henrik@roadhouse.agency>
2018-03-20 22:42:24 +0900

    OPTIONS ajax error fixed

--- TokenHandler.js
+++ TokenHandler.js
@@ -51,8 +51,8 @@ class TokenHandler {
 	 */
 	static validateToken(token, cb) {
 		this.findOneToken({ token: token, expires: { $gt: new Date() } }, (err, tokenObj) => {
-			if(err) return cb(new Error(err));
-			if(!tokenObj) return cb(new Error('Not valid token found!'));
+			if (err) return cb(new Error(err));
+			if (!tokenObj) return cb(new Error('Not valid token found!'));
 			cb(err, tokenObj);
 		});
 	}
@@ -65,29 +65,32 @@ class TokenHandler {
 	 */
 	static tokenMiddleware(req, res, next) {
 		let User = mongoose.model('User');
-
-		if (req.header('authorization')) {
-			// If one exists, attempt to get the header data
-			let token = req.header('authorization').split('Bearer ')[1];
-			// If there's nothing after "Bearer", just redirect to login
-			if (!token) return res.status(401).json({ message: 'No bearer' });
-			// find token
-			Token.findOne({ token: token, expires: { $gt: new Date() } }, (err, tokenObj) => {
-				// If there's an error verifying the token (e.g. it's invalid or expired)
-				if (err || !tokenObj) return res.status(401).json({ message: 'Invalid or expired' });
-				User.findById(tokenObj.user, function (err, user) {
-					// Query error
-					if (err) return res.status(401).json({ message: 'Query error' });
-					// If the user can't be found, redirect to the login page
-					if (!user) return res.status(401).json({ message: 'Could not find user' });
-					// Otherwise save the user object on the request (i.e. "log in") and continue
-					req.user = user;
-					return next();
+		if (req.method ==! 'OPTIONS') {
+			if (req.header('authorization')) {
+				// If one exists, attempt to get the header data
+				let token = req.header('authorization').split('Bearer ')[1];
+				// If there's nothing after "Bearer", just redirect to login
+				if (!token) return res.status(401).json({ message: 'No bearer' });
+				// find token
+				Token.findOne({ token: token, expires: { $gt: new Date() } }, (err, tokenObj) => {
+					// If there's an error verifying the token (e.g. it's invalid or expired)
+					if (err || !tokenObj) return res.status(401).json({ message: 'Invalid or expired' });
+					User.findById(tokenObj.user, function (err, user) {
+						// Query error
+						if (err) return res.status(401).json({ message: 'Query error' });
+						// If the user can't be found, redirect to the login page
+						if (!user) return res.status(401).json({ message: 'Could not find user' });
+						// Otherwise save the user object on the request (i.e. "log in") and continue
+						req.user = user;
+						return next();
+					});
 				});
-			});
+			} else {
+				// No authorization header
+				return res.status(401).json({ message: 'Authorization header missing!' });
+			}
 		} else {
-			// No authorization header
-			return res.status(401).json({ message: 'Authorization header missing!' });
+			next();
 		}
 	}
 
